af4690b2186a9e5dad3071a58b6524a5c00dbd34
Jean-Sebastien Mouret <js.mouret@gmail.com>
2019-05-01 17:14:32 +0800

    Split IContext and IEventContext

---
+++ Assets/Grove/Runtime/Events/EventContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+using Grove.Common;
+using Grove.Variables;
+
+namespace Grove.Events
+{
+	public interface IEventContext : IContext
+	{
+		void OnEvent();
+	}
+}
